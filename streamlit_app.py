a7c2b4cf2c6225c238b63d0e53c6c4ac061bfbc7
taneliorelma <114132992+taneliorelma@users.noreply.github.com>
2023-10-05 12:44:25 +0300

    Update streamlit_app.py
    everything fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('My Parents New Healthy Diner w')
+st.title('My Parents New Healthy Diner')
 st.header('Breakfast menu')
 
 st.text('Omega 3 and Blueberry oatmeal')
