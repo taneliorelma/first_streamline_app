b976f701f7aae992b5a43df12eb73fdcaa82a60b
taneliorelma <114132992+taneliorelma@users.noreply.github.com>
2023-10-06 10:12:25 +0300

    Update streamlit_app.py
    text_input function

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ st.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 st.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
 
 import requests as rq
 
